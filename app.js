f1560658db5515096df52ab192b6dbaab1329105
Shariq Shah <shahshariq47@gmail.com>
2023-05-12 18:29:22 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const calculatetemp =()=>{
+
+    const numberTemp = document.getElementById('temp').value;
+    const tempSelected =document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+
+    const celtoFah =(cel)=>{
+        let Fahrenheit = Math.round((cel*9/5)+ 32);
+        return Fahrenheit;
+    }
+
+    const fahtoCel =(fah)=>{
+        let celsius = Math.round((fah-32) * 5/9);
+        return celsius;
+    }
+
+    let result;
+
+
+    if(valueTemp=='cel'){
+        result =celtoFah(numberTemp);
+        document.getElementById("resultContainer").innerHTML = `= ${result} Fahrenheit`;
+    }else{
+        result =fahtoCel(numberTemp);
+        document.getElementById("resultContainer").innerHTML = `= ${result} Celsius`;
+    }
+}
